dfbb4aa4e1ae47caaddaba58eeadbd266e7f9c4e
mr-car <mr-car@foxmail.com>
2019-07-23 23:07:52 +0800

    项目启动

---
+++ src/pages/mine/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class index extends Component {
+  render() {
+    return (
+      <div>
+        我的信息！！！
+      </div>
+    );
+  }
+}
+
+export default index;
