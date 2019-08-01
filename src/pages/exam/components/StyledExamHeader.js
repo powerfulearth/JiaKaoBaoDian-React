fcd59fa752c4f16b95d3a382c35d547b0b4f841a
songqiang <861085151@qq.com>
2019-08-01 10:58:03 +0800

    test

---
+++ src/pages/exam/components/StyledExamHeader.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const StyledHeaderContainer = styled.div`
+
+`
+
+export {
+  StyledHeaderContainer
+}
---
+++ src/pages/exam/one/One.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import ExamHeader from '../components/ExamHeader'
+
+class One extends Component {
+  render() {
+    return (
+      <div>
+        <ExamHeader></ExamHeader>
+      </div>
+    );
+  }
+}
+
+export default One;
