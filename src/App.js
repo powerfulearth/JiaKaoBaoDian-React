3e4cc8364223cd034d0dc25d67248ef572335efa
车心伟 <mr-car@foxmail.com>
2019-08-09 19:43:50 +0800

    修改

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Index from './pages/index';
-import JiaXiaoDetail from './pages/apply/details/jiaxiaoDetail';
+// import JiaXiaoDetail from './pages/apply/details/jiaxiaoDetail';
 
 import './assets/styles/YoReset.css'
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Route path="/apply" exact component={Index} />
-        <Route path="/apply/jxdetail/" component={JiaXiaoDetail} /> 
-      </Router>
+      // <Router>
+      //   <Route path="/apply" exact component={Index} />
+      //   <Route path="/apply/jxdetail/" component={JiaXiaoDetail} /> 
+      // </Router>
+      <Index></Index>
     );
   }
 }
