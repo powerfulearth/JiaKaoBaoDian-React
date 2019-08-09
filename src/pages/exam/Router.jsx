e2d21f8cead05bc14f6feb563498af5dac4496d2
车心伟 <mr-car@foxmail.com>
2019-08-09 22:51:01 +0800

    11

--- src/pages/exam/Router.jsx
+++ src/pages/exam/Router.jsx
@@ -10,7 +10,6 @@ class Routers extends Component {
   render() {
     return (
       <Router>
-        <Route path="/exam" exact ></Route>
         <Route path="/exam/specialTest" component={SpecialTest}></Route>
         <Route path="/exam/orderPractice" component={TestQuestions}></Route>
         <Route path="/exam/practiceExams" component={PracticeExams}></Route>
